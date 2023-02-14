46b9d1d18e78bbfb8f8923db25d3347249643091
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-02-14 19:33:40 +0900

    Create 230214_숫자의개수2577_백준.swift

---
+++ sunnyCookie/230214_숫자의개수2577_백준.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let a = readLine()!
+let b = readLine()!
+let c = readLine()!
+
+let multipliedABC = Int(a)! * Int(b)! * Int(c)! 
+var result = Array(repeating: 0, count: 10)
+
+for index in String(multipliedABC) {
+    result[Int(String(index))!] += 1
+}
+
+result.forEach { number in
+    print(number)
+}
+
+//Character -> Int 형변환시 String으로 형변환 후 가능 Character -> String -> Int
