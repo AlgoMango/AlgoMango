afe8c42e7f0c15ebf887084b30346cb9a8655dc9
Hyo <59204352+KyoPak@users.noreply.github.com>
2023-02-19 11:42:15 +0900

    Create 230219_올바른괄호_프로그래머스.swift

---
+++ Kyo/230219_올바른괄호_프로그래머스.swift
@@ -0,0 +1,18 @@
+//230219_올바른괄호_프로그래머스
+
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var flag = 0
+    for item in s {
+        if item == "(" {
+            flag += 1
+        } else {
+            flag -= 1
+        }
+        if flag < 0 { return false }
+    }
+    
+    return flag == 0 ? true : false
+}
