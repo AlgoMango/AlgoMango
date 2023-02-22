75e7f2c59a322eaf0a6bc593652ceb5a188d3f85
Hyo <59204352+KyoPak@users.noreply.github.com>
2023-02-23 00:19:21 +0900

    Update and rename ❌230221_2493탑_백준.swift to 230221_2493탑_백준🖍.swift

--- Kyo/230221_2493탑_백준🖍.swift
+++ Kyo/230221_2493탑_백준🖍.swift
@@ -27,3 +27,26 @@ result.append(0)
 for item in Array(result.reversed()) {
     print(item, terminator: " ")
 }
+
+
+//230223_2493탑_백준
+
+let testCase = Int(readLine()!)!
+let numbers = readLine()!.split(separator: " ").map{ Int(String($0))! }
+
+var stack: [(Int, Int)] = [(100000001, 0)]
+var result: [Int] = []
+
+for index in 0..<numbers.count {
+    
+    while stack.last!.0 < numbers[index] {
+        stack.removeLast()
+    }
+    
+    result.append(stack.last!.1)
+    stack.append((numbers[index], index + 1))
+}
+
+for item in result {
+    print(item, terminator:  " ")
+}
