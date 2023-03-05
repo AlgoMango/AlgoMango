a99e10bf36a760cd9ab97ac1f38a056b5dcf74a5
summercat <98260324+dev-summer@users.noreply.github.com>
2023-03-05 14:42:06 +0900

    Update 230305_프로그래머스_43165_타겟넘버.swift

--- summercat/230305_프로그래머스_43165_타겟넘버.swift
+++ summercat/230305_프로그래머스_43165_타겟넘버.swift
@@ -29,3 +29,43 @@ func recur(_ idx: Int, _ cur: Int) {
   recur(idx + 1, cur + numbersCopy[idx])
   recur(idx + 1, cur - numbersCopy[idx])
 }
+
+
+
+// dfs (예전에 푼 풀이)
+func solution(_ numbers: [Int], _ target: Int) -> Int {
+    var graph = Array(repeating: Array(repeating: 0, count: 2), count: numbers.count + 1)
+    var visited = Array(repeating: false, count: numbers.count + 1)
+    var sum = 0
+    var count = 0
+    
+    for i in 0..<numbers.count {
+        graph[i][0] = numbers[i]
+        graph[i][1] = -numbers[i]
+    }
+    print(graph)
+    
+    func dfs(depth: Int) {
+        if depth == numbers.count && sum == target {
+            count += 1
+        }
+        if depth == numbers.count { return }
+        
+        for i in 0...depth {
+            if visited[i] == false {
+                visited[i] = true
+                for j in 0..<2 {
+                    sum += graph[i][j]
+                    dfs(depth: depth + 1)
+                    sum -= graph[i][j]
+                }
+                visited[i] = false
+            }
+            
+        }
+    }
+    
+    dfs(depth: 0)
+    
+    return count
+}
