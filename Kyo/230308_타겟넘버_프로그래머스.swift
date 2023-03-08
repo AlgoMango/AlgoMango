ecf63f2c5f850e9d7b87262b3707351a48d012ac
Hyo <59204352+KyoPak@users.noreply.github.com>
2023-03-09 00:41:35 +0900

    Create 230308_타겟넘버_프로그래머스.swift

---
+++ Kyo/230308_타겟넘버_프로그래머스.swift
@@ -0,0 +1,22 @@
+//230308_타겟넘버_프로그래머스
+
+import Foundation
+
+var answer = 0
+
+func solution(_ numbers:[Int], _ target:Int) -> Int {
+    recursiveAdd(numbers, target, 0, 0)
+    return answer
+}
+
+func recursiveAdd(_ numbers: [Int], _ target: Int, _ index: Int, _ result: Int) {
+    if index == numbers.count {
+        if result == target {
+            answer += 1
+        }
+        return 
+    }
+    
+    recursiveAdd(numbers, target, index + 1, result + numbers[index])
+    recursiveAdd(numbers, target, index + 1, result - numbers[index])
+}
