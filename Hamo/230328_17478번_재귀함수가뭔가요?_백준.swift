1fb0974adc514b5d11d801238e0119f029f25f1d
Hamo <85005933+lxodud@users.noreply.github.com>
2023-03-29 01:11:01 +0900

    Create 230328_17478번_재귀함수가뭔가요?_백준.swift

---
+++ Hamo/230328_17478번_재귀함수가뭔가요?_백준.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+let count = Int(readLine()!)!
+var underBar = "____"
+
+print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+
+func chatBot(currentCount: Int) {
+    printUnderBar(count: currentCount)
+    print("\"재귀함수가 뭔가요?\"")
+    if currentCount == count {
+        printUnderBar(count: currentCount)
+        print("\"재귀함수는 자기 자신을 호출하는 함수라네\"")
+        printUnderBar(count: currentCount)
+        print("라고 답변하였지.")
+        return
+    }
+    
+    printUnderBar(count: currentCount)
+    print("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.")
+    printUnderBar(count: currentCount)
+    print("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
+    printUnderBar(count: currentCount)
+    print("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
+    chatBot(currentCount: currentCount + 1)
+    
+    printUnderBar(count: currentCount)
+    print("라고 답변하였지.")
+}
+
+func printUnderBar(count: Int) {
+    if count == 0 {
+        return
+    }
+    
+    for _ in 1...count {
+        print("____", terminator: "")
+    }
+}
+
+chatBot(currentCount: 0)
