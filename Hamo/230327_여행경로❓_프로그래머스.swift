dcb43b85abe2fc77564de32232053ceaae671414
Hamo <85005933+lxodud@users.noreply.github.com>
2023-04-05 02:39:02 +0900

    Update 230327_여행경로❓_프로그래머스.swift

--- Hamo/230327_여행경로❓_프로그래머스.swift
+++ Hamo/230327_여행경로❓_프로그래머스.swift
@@ -1,42 +1,48 @@
 import Foundation
 
 func solution(_ tickets:[[String]]) -> [String] {
-    var graph: [String: [String]] = [:]
-    var result: [(String, Int)] = []
+    var graph: [String: [(String, Bool)]] = [:]
+    var result: [[String]] = []
+    var isFinish = false
     
     for arr in tickets {
         if graph[arr[0]] != nil {
-            graph[arr[0]]?.append(arr[1])
+            graph[arr[0]]?.append((arr[1], false))
         } else {
-            graph[arr[0]] = [arr[1]]
+            graph[arr[0]] = [(arr[1], false)]
         }
     }
     
     for (key, value) in graph {
-        graph[key] = value.sorted { $0.first! > $1.first! }
+        graph[key] = value.sorted { $0.0.first! < $1.0.first! }
     }
     
-    var needVisitStack: [(String, Int)] = [("ICN", 0)]
-    
-    while !needVisitStack.isEmpty {
-        let currentNode = needVisitStack.removeLast()
+    func dfs(current: String, depth: Int, route: [String]) {
+        var copyRoute = route
+        if depth == tickets.count + 1 {
+            copyRoute.append(current)
+            result.append(copyRoute)
+            return
+        }
         
-        if !result.isEmpty && result.last!.1 >= currentNode.1 {
-            while result.last!.1 >= currentNode.1 {
-                result.removeLast()
+        copyRoute.append(current)
+        
+        guard let copyGraph = graph[current] else {
+            return
+        }
+        
+        if !isFinish {
+            for index in copyGraph.indices {
+                if graph[current]![index].1 == false && !isFinish {
+                    graph[current]![index].1 = true
+                    dfs(current: graph[current]![index].0, depth: depth + 1, route: copyRoute)
+                    graph[current]![index].1 = false
+                }
             }
         }
-        
-        result.append((currentNode.0, currentNode.1))
-        
-        if graph[currentNode.0] != nil {
-            needVisitStack += graph[currentNode.0]!.map { ($0, currentNode.1 + 1) }
-        }
-        
-        if result.count == tickets.count + 1 {
-            break
-        }
     }
     
-    return result.map { $0.0 }
+    dfs(current: "ICN", depth: 1, route: [])
+    
+    return result.filter { $0.count == tickets.count + 1}[0]
 }
