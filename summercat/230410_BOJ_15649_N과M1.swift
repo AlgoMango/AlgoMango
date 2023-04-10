f2ac4c219942ddbc2de800a0e9bb2dee9c8bf0b9
summercat <98260324+dev-summer@users.noreply.github.com>
2023-04-10 15:21:35 +0900

    Create 230410_BOJ_15649_N과M1.swift

---
+++ summercat/230410_BOJ_15649_N과M1.swift
@@ -0,0 +1,26 @@
+let input = readLine()!.split(separator: " ").compactMap { Int($0) }
+let n = input[0]
+let m = input[1]
+var used = Array(repeating: false, count: n + 1)
+
+func recur(_ arr: [Int]) {
+  if arr.count == m {
+    let line: String = arr.map { String($0) }.joined(separator: " ")
+    print(line)
+  }
+  
+  for i in 1...n {
+    if used[i] { continue }
+    var next: [Int] = arr
+    next.append(i)
+    used[i] = true
+    recur(next)
+    used[i] = false
+  }
+}
+
+for i in 1...n {
+  used[i] = true
+  recur([i])
+  used[i] = false
+}
