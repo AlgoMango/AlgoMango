a9978b3fc2f61a2ddbf34da299311d7d20158758
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-04-17 23:08:38 +0900

    Create 230417_수정렬하기2_2751_백준.swift

---
+++ 230417_수정렬하기2_2751_백준.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+// https://www.acmicpc.net/problem/2751
+// mergeSort 사용
+
+let n: Int = Int(readLine()!)!
+var numbers: [Int] = []
+
+for _ in 0..<n {
+    let number: Int = Int(readLine()!)!
+    numbers.append(number)
+}
+
+func mergeSort(_ numbers: [Int]) -> [Int] {
+    guard numbers.count > 1 else { return numbers }
+
+    let mid = numbers.count / 2
+    let left = Array(numbers[0..<mid])
+    let right = Array(numbers[mid..<numbers.count])
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+func merge(_ left: [Int], _ right: [Int]) -> [Int] {
+    var leftIdx: Int = 0
+    var rightIdx: Int = 0
+    var merged = [Int]()
+
+    while true {
+        if leftIdx == left.count {
+            merged += right[rightIdx..<right.count]
+            break
+        }
+
+        if rightIdx == right.count {
+            merged += left[leftIdx..<left.count]
+            break
+        }
+
+        if left[leftIdx] < right[rightIdx] {
+            merged.append(left[leftIdx])
+            leftIdx += 1
+        } else {
+            merged.append(right[rightIdx])
+            rightIdx += 1
+        }
+    }
+
+    return merged
+}
+
+mergeSort(numbers).forEach { print($0) }
